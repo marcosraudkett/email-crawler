e8cba4e9a2519e54f3971c126e9db8237f6a5ef4
Marcos Raudkett <info@marcosraudkett.com>
2019-01-15 00:24:55 +0200

    Create init.php

---
+++ includes/init.php
@@ -0,0 +1,26 @@
+<?php 
+
+  ob_start();
+  session_start();
+
+  error_reporting(E_ALL);
+
+  /**
+   * Initialisations
+   */
+
+  // Register autoload function
+  spl_autoload_register('myAutoloader');
+
+  /**
+   * Autoloader
+   *
+   * @param string $className  The name of the class
+   * @return void
+   */
+  function myAutoloader($className)
+  {
+    require dirname(dirname(__FILE__)) . '/classes/' . $className . '.class.php';
+  }
+
+?>
